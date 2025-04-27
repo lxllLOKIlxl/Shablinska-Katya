452b71fb85aa0c11464b28403d2a8817c0014e68
Shablinskii Stanislav Ivanovith <lxlllokillxl@gmail.com>
2025-04-27 13:13:56 +0300

    script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.getElementById("changeBg").addEventListener("click", function() {
-    document.body.style.backgroundColor = "#" + Math.floor(Math.random()*16777215).toString(16);
+    document.body.style.backgroundColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
 });
 
 document.getElementById("changeImg").addEventListener("click", function() {
