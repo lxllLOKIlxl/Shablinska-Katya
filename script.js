13364c9436befcd36111182203375ede06d03bf8
Shablinskii Stanislav Ivanovith <lxlllokillxl@gmail.com>
2025-04-27 12:33:20 +0300

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+document.getElementById("changeBg").addEventListener("click", function() {
+    document.body.style.backgroundColor = "#" + Math.floor(Math.random()*16777215).toString(16);
+});
 
+document.getElementById("changeImg").addEventListener("click", function() {
+    document.getElementById("image").src = "https://placebear.com/300/200";
+});
